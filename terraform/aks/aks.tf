2f2ebce9f4d8d50ae025ff47a2435107cbd60151
Esteban Pulgarin Arango <esteban-pulgarin.a@gmail.com>
2024-05-26 22:52:49 -0500

    terraform

--- terraform/aks/aks.tf
+++ terraform/aks/aks.tf
@@ -13,7 +13,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg_reto_devsecops"
     storage_account_name = "saretodevsecops"
-    container_name       = "scretodevsecopsaks"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
 }
