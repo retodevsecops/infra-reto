0d28f9eab1e27b2b7e39618858bb339d209cb4e5
Esteban Pulgarin Arango <esteban-pulgarin.a@gmail.com>
2024-05-26 23:49:10 -0500

    terraform

--- terraform/aks/output.tf
+++ terraform/aks/output.tf
@@ -1,21 +1,25 @@
 output "client_key" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+    sensitive = true
 }
 
 output "client_certificate" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+    sensitive = true
 }
 
 output "cluster_ca_certificate" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+    sensitive = true
 }
 
 output "cluster_username" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+    value = nonsensitive(azurerm_kubernetes_cluster.k8s.kube_config.0.username)
 }
 
 output "cluster_password" {
     value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+    sensitive = true
 }
 
 output "kube_config" {
@@ -24,5 +28,6 @@ output "kube_config" {
 }
 
 output "host" {
-    value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+    value = nonsensitive(azurerm_kubernetes_cluster.k8s.kube_config.0.host)
+    
 }
