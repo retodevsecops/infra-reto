567fbc6bff7618bf61db56851100f39cdcc0e1e7
Esteban Pulgarin Arango <esteban-pulgarin.a@gmail.com>
2024-05-26 19:54:12 -0500

    variables

--- az/az-storages.sh
+++ az/az-storages.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 az group exists -n rg_reto_devsecops
-STACCOUNT=$(az storage account check-name --name $STORAGEACCOUNTNAME)
-VALUE=$(jq -r '.nameAvailable' <<< $STACCOUNT)
+az account show
+az storage -h
+# STACCOUNT=$(az storage account check-name --name $STORAGEACCOUNTNAME)
+# VALUE=$(jq -r '.nameAvailable' <<< $STACCOUNT)
 
-if [[ "$VALUE" == "true" ]]; then
-    az storage account create \
-    --name $STORAGEACCOUNTNAME \
-    --resource-group $RESOURCEGROUP \
-    --location $LOCATION \
-    --sku Standard_RAGRS \
-    --kind StorageV2   
+# if [[ "$VALUE" == "true" ]]; then
+#     az storage account create \
+#     --name $STORAGEACCOUNTNAME \
+#     --resource-group $RESOURCEGROUP \
+#     --location $LOCATION \
+#     --sku Standard_RAGRS \
+#     --kind StorageV2   
 
-    JSON=$(az storage account keys list \
-    --resource-group $RESOURCEGROUP \
-    --account-name $STORAGEACCOUNTNAME)
+#     JSON=$(az storage account keys list \
+#     --resource-group $RESOURCEGROUP \
+#     --account-name $STORAGEACCOUNTNAME)
 
-    KEYST=( $(jq -r '.[0].value' <<< $JSON) ) 
+#     KEYST=( $(jq -r '.[0].value' <<< $JSON) ) 
                                 
-    az storage container create -n $STORAGECONTAINERNAMEACR --account-name $STORAGEACCOUNTNAME --account-key $KEYST        
-    az storage container create -n $STORAGECONTAINERNAMEAKS --account-name $STORAGEACCOUNTNAME --account-key $KEYST
-else
+#     az storage container create -n $STORAGECONTAINERNAMEACR --account-name $STORAGEACCOUNTNAME --account-key $KEYST        
+#     az storage container create -n $STORAGECONTAINERNAMEAKS --account-name $STORAGEACCOUNTNAME --account-key $KEYST
+# else
 
-    JSON=$(az storage account keys list \
-    --resource-group $RESOURCEGROUP \
-    --account-name $STORAGEACCOUNTNAME)
+#     JSON=$(az storage account keys list \
+#     --resource-group $RESOURCEGROUP \
+#     --account-name $STORAGEACCOUNTNAME)
 
-    KEYST=( $(jq -r '.[0].value' <<< $JSON) ) 
+#     KEYST=( $(jq -r '.[0].value' <<< $JSON) ) 
                                 
-    az storage container create -n $STORAGECONTAINERNAMEACR --account-name $STORAGEACCOUNTNAME --account-key $KEYST        
-    az storage container create -n $STORAGECONTAINERNAMEAKS --account-name $STORAGEACCOUNTNAME --account-key $KEYST
-fi
+#     az storage container create -n $STORAGECONTAINERNAMEACR --account-name $STORAGEACCOUNTNAME --account-key $KEYST        
+#     az storage container create -n $STORAGECONTAINERNAMEAKS --account-name $STORAGEACCOUNTNAME --account-key $KEYST
+# fi
